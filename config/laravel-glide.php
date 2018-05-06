f820502e3644f0a2c8643eb76e497ac74f4490f6
Ilyas Foo <ilyas@web2square.com>
2018-05-06 23:36:47 +0800

    Fixed config

--- config/laravel-glide.php
+++ config/laravel-glide.php
@@ -6,4 +6,10 @@ return [
      * The driver that will be used to create images. Can be set to gd or imagick.
      */
     'driver' => 'gd',
+
+    /*
+     * Storage key that will be used for source file according to filesystem config. 
+     * Your laravel default should have local, public, and s3.
+     */
+    'source_storage' => 'local',
 ];
